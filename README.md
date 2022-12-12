7aff181e35453d5ade07c4535ae03ad8f735ac03
esprimo <johan.edholm90@gmail.com>
2022-12-12 21:31:21 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
 # bash-ip-calculator
-Calculate network ranges, CIDRs etc. Written in pure bash
+
+Calculate network ranges, CIDRs etc. Written in pure Bash. I probably wrote it around 2015 and found it now while wiping an old laptop. Made for fun rather than being useful.
+
+```
+$ ./ipcalc.sh -h
+Usage:
+ipcalc.sh [OPTIONS] NETWORK
+  -r,--print-ip-range	Print all IPs in the range, one per line.
+  -h,--help				Print this help message.
+
+NETWORK is an IP and a prefix or netmark separated with a slash. For example:
+- 192.168.0.1/24
+- 192.168.0.1/255.255.255.0
+```
+
+Example using CIDR:
+
+```
+$ ./ipcalc.sh 192.168.1.13/27
+Address (dec): 192.168.1.13
+Address (bin): 11000000 10101000 00000001 00001101
+Wildcard mask (dec): 0.0.0.31
+Wildcard mask (bin): 00000000 00000000 00000000 00011111
+Netmask (dec): 255.255.255.224
+Netmask (bin): 11111111 11111111 11111111 11100000
+Prefix: 27
+Number of hosts: 30
+CIDR: 192.168.1.0/27
+Broadcast: 192.168.1.31
+```
+
+Example using IP and netmask:
+
+```
+$ ./ipcalc.sh 10.0.2.0/255.255.255.0
+Address (dec): 10.0.2.0
+Address (bin): 00001010 00000000 00000010 00000000
+Wildcard mask (dec): 0.0.0.255
+Wildcard mask (bin): 00000000 00000000 00000000 11111111
+Netmask (dec): 255.255.255.0
+Netmask (bin): 11111111 11111111 11111111 00000000
+Prefix: 24
+Number of hosts: 254
+CIDR: 10.0.2.0/24
+Broadcast: 10.0.2.255
+```
+
+Example printing all IPs:
+
+```
+$ ./ipcalc.sh --print-ip-range 127.0.0.0/29
+Address (dec): 127.0.0.0
+Address (bin): 01111111 00000000 00000000 00000000
+Wildcard mask (dec): 0.0.0.7
+Wildcard mask (bin): 00000000 00000000 00000000 00000111
+Netmask (dec): 255.255.255.248
+Netmask (bin): 11111111 11111111 11111111 11111000
+Prefix: 29
+Number of hosts: 6
+CIDR: 127.0.0.0/29
+Broadcast: 127.0.0.7
+127.0.0.0
+127.0.0.1
+127.0.0.2
+127.0.0.3
+127.0.0.4
+127.0.0.5
+127.0.0.6
+127.0.0.7
+```
+
+Example using multilpe networks:
+
+```
+$ ./ipcalc.sh 192.168.1.13/27 10.0.2.0/255.255.255.0
+Address (dec): 192.168.1.13
+Address (bin): 11000000 10101000 00000001 00001101
+Wildcard mask (dec): 0.0.0.31
+Wildcard mask (bin): 00000000 00000000 00000000 00011111
+Netmask (dec): 255.255.255.224
+Netmask (bin): 11111111 11111111 11111111 11100000
+Prefix: 27
+Number of hosts: 30
+CIDR: 192.168.1.0/27
+Broadcast: 192.168.1.31
+
+Address (dec): 10.0.2.0
+Address (bin): 00001010 00000000 00000010 00000000
+Wildcard mask (dec): 0.0.0.255
+Wildcard mask (bin): 00000000 00000000 00000000 11111111
+Netmask (dec): 255.255.255.0
+Netmask (bin): 11111111 11111111 11111111 00000000
+Prefix: 24
+Number of hosts: 254
+CIDR: 10.0.2.0/24
+Broadcast: 10.0.2.255
+```
---
+++ ipcalc.sh
@@ -0,0 +1,236 @@
+#!/usr/bin/env bash
+
+## Convert a network prefix and echoes the binary equalient in octets
+# @param prefix to convert
+# @result bitmask in octets
+prefix_to_netmaskBin() {
+    local prefix="$1"
+    local bitbase='11111111111111111111111111111111'
+    local ones
+    local netmaskBin
+
+    # print $prefix amount of space and save to $spaces
+    printf -v spaces '%*s' $((32 - prefix))
+    # replace space with zeros
+    ones=${spaces// /0}
+
+    # the prefix number is how many ones that is needed, so make take a
+    # substring of that length of the bitbase and append the zeroes
+    netmaskBin="${bitbase:0:${prefix}}${ones}"
+
+    # put spaces between every 8th character (return octets)
+    echo "${netmaskBin:0:8} ${netmaskBin:8:8} ${netmaskBin:16:8} ${netmaskBin:24:8}"
+}
+
+## Convert a binary netmask to decimal wildcard separated with space
+# @param binary netmask in octets
+# @result space separated wild card mask in decimal form
+netmaskBin_to_wildcard() {
+    local netmaskBin="$1"
+    local wildcard_mask
+
+    for octet in $netmaskBin; do
+        wildcard_mask="${wildcard_mask}${wildcard_mask:+.}$((255 - 2#$octet))"
+    done
+
+    echo "$wildcard_mask"
+}
+
+## Convert binary ip to decimal ip
+# @param binary ip in octets separated with space
+# @result dot separated ip
+# netmask bin = rätt, netmask to ip = wrong last octet for 32
+binary_to_ip() {
+    local binary="$1"
+    local ip
+
+    for octet in $binary; do
+        ip="${ip}${ip:+.}$((2#$octet))"
+    done
+
+    echo "$ip"
+}
+
+## Convert an integer to binary
+# @param integer to convert
+# @result binary representation of $1
+int_to_bin() {
+    local int="$1"
+    local binary
+
+    # not the most optimal way but it works
+    for i in {0..7}; do
+        binary="$((int % 2))$binary"
+        int=$((int / 2))
+    done
+
+    echo "$binary"
+}
+
+ip_to_binary() {
+    local ip="$1"
+    local binary
+
+    IFS='.' read -ra ip_octet <<<"$ip"
+    for i in {0..3}; do
+        binary="${binary}${binary:+ }$(int_to_bin ${ip_octet[i]})"
+    done
+
+    echo "$binary"
+}
+
+## Makes IP ranges in bash suntax (eg 192.168.0.{0..255} for 192.168.0.0/24)
+# @param wildcard_mask a space separated wildcard mask in decimal form
+# (eg 0 0 0 255)
+# @param dot separated network address (eg 192.168.0.0)
+# @result IP range that bash can expland (eg 192.168.0.{0..255})
+generate_bash_range() {
+    local wildcard_mask="$1"
+    local net="$2"
+    local net_range
+    local octet_range
+
+    IFS='.' read -ra net_octet <<<"$net"
+    IFS='.' read -ra wildcard_octet <<<"$wildcard_mask"
+
+    for i in {0..3}; do
+        if [ "${wildcard_octet[i]}" -gt 0 ]; then
+            octet_range="{0..${wildcard_octet[i]}}"
+        else
+            octet_range="${net_octet[i]}"
+        fi
+        net_range="${net_range}${net_range:+.}${octet_range}"
+    done
+
+    echo "$net_range"
+}
+
+is_valid_ip() {
+    local ip="$1"
+
+    IFS='.' read -ra ip_octet <<<"$ip"
+
+    # An IP must always be 4 octes
+    if [ ${#ip_octet[@]} -ne 4 ]; then
+        return 1
+    fi
+
+    for i in {0..3}; do
+        # Check if the octet is a number
+        if [ -n "${ip_octet[i]//[0-9]/}" ]; then
+            return 1
+        # The octet must be between 0 and 255
+        elif [ "${ip_octet[i]}" -gt 255 ] || [ "${ip_octet[i]}" -lt 0 ]; then
+            return 1
+        fi
+    done
+
+    return 0
+}
+
+die() {
+    echo "Error: $*" >&2
+    exit 1
+}
+
+usage() {
+    cat <<EOF
+Usage:
+$(basename "$0") [OPTIONS] NETWORK
+  -r,--print-ip-range   Print all IPs in the range, one per line.
+  -h,--help             Print this help message.
+
+NETWORK is an IP and a prefix or netmark separated with a slash. For example:
+- 192.168.0.1/24
+- 192.168.0.1/255.255.255.0
+EOF
+
+}
+
+# Set default values for options
+print_ip_range="${PRINT_IP_RANGE=false}"
+
+# Process all the command line options
+while [ $# -gt 0 ]; do
+    case $1 in
+    # Two hyphens ends the options parsing
+    --)
+        shift
+        break
+        ;;
+    -h | --help)
+        usage
+        exit
+        ;;
+    -r | --print-ip-range) print_ip_range=true ;;
+    # Anything remaining that starts with a dash triggers a fatal error
+    -?*)
+        usage
+        die "Option '$1' is unknown"
+        ;;
+    # Anything remaining is treated as content not a parseable option
+    *)
+        break
+        ;;
+    esac
+    shift
+done
+
+for network in "$@"; do
+    # Address
+    address=${network%%/*}
+    if ! is_valid_ip "$address"; then
+        die "Invalid IP: $address"
+    fi
+    addressBin=$(ip_to_binary "$address")
+
+    # Prefix/netmask
+    prefix=${network##*/}
+    if is_valid_ip "$prefix"; then
+        netmaskBin=$(ip_to_binary "$prefix")
+    elif [ -z "${prefix//[0-9]/}" ]; then
+        netmaskBin=$(prefix_to_netmaskBin "$prefix")
+    else
+        die "Invalid prefix or netmask: $prefix"
+    fi
+    netmaskDec=$(binary_to_ip "$netmaskBin")
+
+    # Wildcard
+    wildcard_mask=$(netmaskBin_to_wildcard "$netmaskBin")
+    wildcard_maskBin=$(ip_to_binary "$wildcard_mask")
+    wildcard_zeros=${wildcard_maskBin//[ 1]/}
+    prefix=${#wildcard_zeros}
+
+    # IP range list
+    ip_with_range=$(generate_bash_range "$wildcard_mask" "$address")
+    ip_list=($(eval echo "$ip_with_range"))
+    broadcast=${ip_list[-1]}
+
+    if [ ${#ip_list[@]} -gt 2 ]; then
+        number_of_hosts=$((${#ip_list[@]} - 2))
+    else
+        # This isn't really a network
+        number_of_hosts=1
+    fi
+
+    cat <<-EOF
+        Address (dec): $address
+        Address (bin): $addressBin
+        Wildcard mask (dec): $wildcard_mask
+        Wildcard mask (bin): $wildcard_maskBin
+        Netmask (dec): $netmaskDec
+        Netmask (bin): $netmaskBin
+        Prefix: $prefix
+        Number of hosts: $number_of_hosts
+        CIDR: ${ip_list[0]}/${prefix}
+        Broadcast: $broadcast
+    EOF
+
+    if [[ "$print_ip_range" = true ]]; then
+        for ip in "${ip_list[@]}"; do
+            echo "$ip"
+        done
+    fi
+
+    echo
+done
